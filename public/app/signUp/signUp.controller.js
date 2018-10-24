942505d8f441e0789ac14fb92a075af8153ebd91
tricong2011 <tricong2011@gmail.com>
2018-10-25 01:33:22 +0700

    fix

--- public/app/signUp/signUp.controller.js
+++ public/app/signUp/signUp.controller.js
@@ -1,10 +1,27 @@
+const mysql = require('mysql') ;
+var mysqlConnection = mysql.createConnection({
+	host = 'localhost',
+	user = 'root',
+	password = '12345678',
+	database = 'hjc_database'
+});
+
 myApp.controller('SignUpController', ['$location','$scope', '$state', '$http','Auth',
   function ($location,$scope, $state, $http, Auth) {
-		$scope.register = function(){		
-				$location.path("\home") ;
-				Auth.showLoginModal();			
+		$scope.register = function(){	
+			   	mysqlConnection.connect(() =>{
+					   if(err){
+							alert("connectErr");
+					   }
+					   else{
+						   alert("success");
+					   }
+				})
+				   
+				// $location.path("\home") ;
+				// Auth.showLoginModal();			
 		}	
-		jQuery("#register-back-btn").click(function () {
+		jQuery("#register-back-btn").click(function () {			
 			$location.path("\home");
 		})
 // 	$(".register-form input").keypress(function (e) { return 13 == e.which ? ($(".register-form").validate().form() && $(".register-form").submit(), !1) : void 0 }),
