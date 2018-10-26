0fa81dd36d8bea29e1779511200737a5110ec592
duongnv <vanduong18598@gmail.com>
2018-10-27 00:05:44 +0700

    fix

--- api/controller/project-controller.js
+++ api/controller/project-controller.js
@@ -68,9 +68,9 @@ exports.create_project = (req, res, next) => {
   file_index = project_path + '/' + "index.html";
   folder_js = project_path + '/' + "javascript";
   file_config = folder_js + '/' + "config.js";
-  file_yjs = folder_js + '/' + "your_js.js";
+  file_yjs = folder_js + '/' + "my_script.js";
   folder_css = project_path + '/' + "style";
-  file_css = folder_css + '/' + "style.css";
+  file_css = folder_css + '/' + "my_style.css";
 
   fs.mkdir(project_path, function (error) {
     if (error) {
@@ -79,7 +79,7 @@ exports.create_project = (req, res, next) => {
       })
     }
     else {
-      fs.writeFileSync(file_index, '<!DOCTYPE html>\n<html>\n  <head>\n    <link rel="stylesheet" href="style/style.css" type="text/css"/>\n  </head>\n  <body>\n\t\t<div id="title">Hello world</div>\n    <script src="javascript/config.js" type="text/javascript"></script>\n  </body>\n</html>\n');
+      fs.writeFileSync(file_index, '<!DOCTYPE html>\n<html>\n  <head>\n    <link rel="stylesheet" href="style/my_style.css" type="text/css"/>\n  </head>\n  <body>\n\t\t<div id="title">Hello world</div>\n    <script src="javascript/config.js" type="text/javascript"></script>\n    <script src="javascript/my_script.js" type="text/javascript"></script>\n  </body>\n</html>\n');
       fs.mkdirSync(folder_js);
       fs.writeFileSync(file_config, 'window.console.log = function (obj) {\n  if (typeof obj === "object")\n  {\n    obj = JSON.stringify(obj);\n  }\n  message = {\n    type: "log-msg",\n    content: obj\n  }\n\twindow.parent.postMessage(message, \'*\');\n}\n\nwindow.onerror = function(msg, url,line) {\n  arr = url.split("/");\n  error = {\n    position: arr[arr.length-1] + ":" + line,\n    content: msg,\n    type: "error-msg"\n  }\n  window.parent.postMessage(error, \'*\');\n}\n');
       fs.writeFileSync(file_yjs, ' ');
