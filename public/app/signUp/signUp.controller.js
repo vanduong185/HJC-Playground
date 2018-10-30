7a4e0cff4be242e4f42c42d0def0d58f9069712b
tricong2011 <tricong2011@gmail.com>
2018-10-30 16:53:33 +0700

    signUp and login

--- public/app/signUp/signUp.controller.js
+++ public/app/signUp/signUp.controller.js
@@ -1,29 +1,24 @@
-const mysql = require('mysql') ;
-var mysqlConnection = mysql.createConnection({
-	host = 'localhost',
-	user = 'root',
-	password = '12345678',
-	database = 'hjc_database'
-});
-
 myApp.controller('SignUpController', ['$location','$scope', '$state', '$http','Auth',
   function ($location,$scope, $state, $http, Auth) {
+	    $('#pass').hide() ;
 		$scope.register = function(){	
-			   	mysqlConnection.connect(() =>{
-					   if(err){
-							alert("connectErr");
-					   }
-					   else{
-						   alert("success");
-					   }
-				})
-				   
-				// $location.path("\home") ;
-				// Auth.showLoginModal();			
-		}	
-		jQuery("#register-back-btn").click(function () {			
-			$location.path("\home");
-		})
+			if($scope.password === $scope.rpassword){
+				var infor = {
+					email : $scope.email,
+					nickname : $scope.nickname,
+					password : $scope.password
+				}
+
+				$http.post("/users/signup", infor, function(res) {
+					console.log(res);
+				});
+				$location.path("/home") ;
+				Auth.showLoginModal();	
+			}					
+			else{
+				$("#pass").toggle();
+			}
+		};
 // 	$(".register-form input").keypress(function (e) { return 13 == e.which ? ($(".register-form").validate().form() && $(".register-form").submit(), !1) : void 0 }),
 // 	jQuery("#register-btn").click(function () {
 // 		jQuery(".login-form").hide()
